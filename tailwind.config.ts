5f759cbd3f134ce48bfe9bdd462640b4f22a1630
Ablyazov Dariy <mr96crusher@gmail.com>
2024-12-31 14:47:38 +0100

    finish  services section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,13 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./index.html', './src/**/*.{ts}'],
+  content: ['./index.html', './src/**/*.ts'],
   theme: {
     colors: {
       gray: 'rgba(0,0,0,0.62)',
       white: 'rgb(255,255,255)',
       black: 'rgb(0,0,0)',
       lightgray: 'rgba(255, 255, 255, 0.44)',
-      raspberry: '#530030',
       apricot: '#FFD2BF',
       debug: 'red',
     },
