e9593b3359c3c047011f52faa85ef7efff5219c6
Ablyazov Dariy <mr96crusher@gmail.com>
2025-01-03 17:01:06 +0100

    fix colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,12 +4,9 @@ export default {
   content: ['./index.html', './src/**/*.ts'],
   theme: {
     colors: {
-      gray: 'rgba(0,0,0,0.62)',
-      white: 'rgb(255,255,255)',
-      black: 'rgb(0,0,0)',
-      lightgray: 'rgba(255, 255, 255, 0.44)',
+      white: '#FFFFFFFF',
+      black: '#000000',
       apricot: '#FFD2BF',
-      debug: 'red',
     },
     fontFamily: {
       roboto: ['Roboto', 'serif'],
